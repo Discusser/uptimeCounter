8d6e30c9936c06a8d52457492ef8344841d24015
Discusser <47938380+Discusser@users.noreply.github.com>
2022-07-18 12:03:42 +0200

    Fixed overlapping dates on graph when too many dates were present

--- generateGraph.py
+++ generateGraph.py
@@ -1,9 +1,13 @@
 import re
 from datetime import datetime
+import time
 
 import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import axes
+from matplotlib import dates as mdates
 
-datesReadable = []
+mplDates = []
 dates: [datetime] = []
 datesLength = 0
 durations = []
@@ -30,26 +34,36 @@ if hasBounds:
     askUpperBound()
 
 with open(r"D:\Coding\Python\UptimeCounter\data.txt", 'r') as file:
-    for time in re.findall(r"\d{19}(?!;$)", file.read()):
-        dates.append(int(time) / 1_000_000_000)
-        dt = datetime.fromtimestamp(int(time) / 1_000_000_000)
-        datesReadable.append(dt.strftime("%d %b %Y %H:%M:%S"))
+    for time1 in re.findall(r"\d{19}(?!;$)", file.read()):
+        dates.append(int(time1) / 1_000_000_000)
+        dt = datetime.fromtimestamp(int(time1) / 1_000_000_000)
+        mplDates.append(dt)
 
 if hasBounds:
     dateFrom1 = datetime.strptime(dateFrom, "%d/%m/%Y").timestamp()
     dateTo1 = datetime.strptime(dateTo, "%d/%m/%Y").timestamp()
     dates = [date for date in dates if dateFrom1 <= date <= dateTo1]
-    datesReadable = [datetime.fromtimestamp(date).strftime("%d %b %Y %H:%M:%S") for date in dates]
+    mplDates = [datetime.fromtimestamp(date) for date in dates]
 
 for i in range(0, len(dates), 2):
     for j in range(2):
         durations.append((dates[i + 1] - dates[i]) / 3600)
 
+mplDates.append(datetime.now())
+durations.append((time.time() - dates[-1]) / 3600)
+
+ax: axes.Axes
 fig, ax = plt.subplots()
-plt.plot(datesReadable, durations)
+ax.plot(mplDates, durations)
+ax.xaxis.set_major_formatter(mdates.DateFormatter("%d/%m/%Y"))
+fig.autofmt_xdate()
 plt.xlabel("Date")
-plt.setp(ax.xaxis.get_majorticklabels(), rotation=-45, ha="left", rotation_mode="anchor")
 plt.ylabel("Duration (hours)")
 plt.title("Uptime")
 plt.tight_layout()
 plt.show()
+
+
+
+
+
